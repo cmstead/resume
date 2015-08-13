486446dfb0fd2252b606a38b41eb21a8a8fb57b9
Chris Stead <cmstead@gmail.com>
2015-08-12 22:36:29 -0700

    Added analytics tracking code to resume.

--- build.js
+++ build.js
@@ -11,11 +11,20 @@ function cleanBadChars(documentStr){
 	return updatedDocument
 }
 
+function addAnalytics(documentStr){
+	var trackingCode = fs.readFileSync('./tracking.html', { encoding: 'utf-8' });
+	
+	return documentStr.replace('<body>', '<body>' + '\n' + trackingCode);
+}
+
 console.log('Reading resume doc');
 var finalDoc = fs.readFileSync('./cstead-resume.html', { encoding: 'utf-8'});
 
 console.log('Replacing bad characters');
 finalDoc = cleanBadChars(finalDoc);
+
+console.log('Adding tracking code');
+finalDoc = addAnalytics(finalDoc);
 
 console.log('Writing updated resume');
 fs.writeFileSync('./cstead-resume.html', finalDoc);
